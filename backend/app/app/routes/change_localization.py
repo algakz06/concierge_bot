bf621c003e6d7d6b75071e91b7e11a7fa9e99fb7
algakz06 <77662392+algakz06@users.noreply.github.com>
2024-09-30 11:49:53 +0300

    fix: change_localization

--- backend/app/app/routes/change_localization.py
+++ backend/app/app/routes/change_localization.py
@@ -2,6 +2,7 @@ from aiogram import F, Router
 from aiogram.types import Message
 
 from app.middlewares.get_localization import GetLocalizationMiddleware
+from app.configs.db import database_session_manager
 from app.services.user_service import UserService
 from app.utils.template_builder import render_template
 
@@ -14,7 +15,8 @@ async def change_localization(message: Message, localization: str):
     localization = {"ru": "eng", "eng": "ru"}[localization]
     language = {"ru": "русский", "eng": "english"}
 
-    user_service = UserService()
+    db = await anext(database_session_manager.get_session())
+    user_service = UserService(db)
     await user_service.set_localization(message.from_user.id, localization)
 
     await message.answer(
